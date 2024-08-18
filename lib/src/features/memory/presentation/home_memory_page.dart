8bc976d7ca570712225874d2d8c8ea45f998c298
kyllian404 <kyllianrpro@gmail.com>
2024-08-18 17:47:21 +0200

    All basics elements in home

--- lib/src/features/memory/presentation/home_memory_page.dart
+++ lib/src/features/memory/presentation/home_memory_page.dart
@@ -52,12 +52,12 @@ class _HomeMemoryPageState extends State<HomeMemoryPage> {
       body: Center(
         child: Column(
           children: [
-            //* Zone Daily challenge
+            //* Titre page
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 10.0),
               child: Container(
                 width: 340,
-                height: 200,
+                height: 45,
                 decoration: BoxDecoration(
                   color: AppColors.fondApp,
                   borderRadius: BorderRadius.circular(5),
@@ -78,15 +78,70 @@ class _HomeMemoryPageState extends State<HomeMemoryPage> {
                     ),
                   ],
                 ),
+                child: const Text(
+                  'Memoryze',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily:
+                        'Roboto', // Remplace par une autre police si souhaité
+                    fontSize: 32, // Taille de la police
+                    fontWeight: FontWeight.normal, // Epaisseur du texte
+                    color: Colors.white, // Couleur du texte
+                    letterSpacing: 2, // Espacement des lettres
+                    wordSpacing: 3, // Espacement des mots
+                  ),
+                ),
               ),
             ),
-        
+            //* Zone Daily challenge
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Container(
+                width: 340,
+                height: 180,
+                decoration: BoxDecoration(
+                  color: AppColors.fondApp,
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: const [
+                    BoxShadow(
+                      blurStyle: BlurStyle.inner,
+                      color: Color.fromARGB(127, 255, 255, 255),
+                      offset: Offset(-2, 5),
+                      blurRadius: 4,
+                      spreadRadius: -3,
+                    ),
+                    BoxShadow(
+                      blurStyle: BlurStyle.inner,
+                      color: Color.fromARGB(65, 0, 0, 0),
+                      offset: Offset(2, -5),
+                      blurRadius: 4,
+                      spreadRadius: 0,
+                    ),
+                  ],
+                ),
+                child: const Text(
+                  textAlign: TextAlign.center,
+                  'Daily Challenge',
+                  style: TextStyle(
+                    fontFamily:
+                        'Roboto', // Remplace par une autre police si souhaité
+                    fontSize: 28, // Taille de la police
+                    fontWeight: FontWeight.normal, // Epaisseur du texte
+                    color: Colors.white, // Couleur du texte
+                    letterSpacing: 2, // Espacement des lettres
+                    wordSpacing: 3, // Espacement des mots
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+            ),
+
             Padding(
               padding: const EdgeInsets.only(left: 38.0),
               child: Column(
                 children: [
                   Row(
-                      //* Time Challenge List 
+                    //* Time Challenge List
                     children: [
                       Padding(
                         padding: const EdgeInsets.only(top: 15.0),
@@ -151,7 +206,6 @@ class _HomeMemoryPageState extends State<HomeMemoryPage> {
                           ),
                         ),
                       ),
-                      
                       const Padding(
                         padding: EdgeInsets.only(top: 15.0, left: 5.0),
                         child: Text(
@@ -165,13 +219,12 @@ class _HomeMemoryPageState extends State<HomeMemoryPage> {
                             letterSpacing: 2, // Espacement des lettres
                             wordSpacing: 3, // Espacement des mots
                           ),
-                          
                         ),
                       ),
                     ],
                   ),
                   Row(
-                      //* Match Mania
+                    //* Match Mania
                     children: [
                       Padding(
                         padding: const EdgeInsets.only(top: 15.0),
@@ -239,23 +292,23 @@ class _HomeMemoryPageState extends State<HomeMemoryPage> {
                       const Padding(
                         padding: EdgeInsets.only(top: 15.0, left: 5.0),
                         child: Text(
-                            'Match Mania',
-                            style: TextStyle(
-                              fontFamily:
-                                  'Roboto', // Remplace par une autre police si souhaité
-                              fontSize: 24, // Taille de la police
-                              fontWeight: FontWeight.normal, // Epaisseur du texte
-                              color: AppColors.iconsPrimary, // Couleur du texte
-                              letterSpacing: 2, // Espacement des lettres
-                              wordSpacing: 3, // Espacement des mots
-                            ),
-                            textAlign: TextAlign.center, // Alignement du texte
+                          'Match Mania',
+                          style: TextStyle(
+                            fontFamily:
+                                'Roboto', // Remplace par une autre police si souhaité
+                            fontSize: 24, // Taille de la police
+                            fontWeight: FontWeight.normal, // Epaisseur du texte
+                            color: AppColors.iconsPrimary, // Couleur du texte
+                            letterSpacing: 2, // Espacement des lettres
+                            wordSpacing: 3, // Espacement des mots
                           ),
+                          textAlign: TextAlign.center, // Alignement du texte
+                        ),
                       ),
                     ],
                   ),
                   Row(
-                      //* Sequence Master
+                    //* Sequence Master
                     children: [
                       Padding(
                         padding: const EdgeInsets.only(top: 15.0),
@@ -323,18 +376,18 @@ class _HomeMemoryPageState extends State<HomeMemoryPage> {
                       const Padding(
                         padding: EdgeInsets.only(top: 15.0, left: 5.0),
                         child: Text(
-                            'Sequence Master',
-                            style: TextStyle(
-                              fontFamily:
-                                  'Roboto', // Remplace par une autre police si souhaité
-                              fontSize: 24, // Taille de la police
-                              fontWeight: FontWeight.normal, // Epaisseur du texte
-                              color: AppColors.iconsPrimary, // Couleur du texte
-                              letterSpacing: 2, // Espacement des lettres
-                              wordSpacing: 3, // Espacement des mots
-                            ),
-                            textAlign: TextAlign.center, // Alignement du texte
+                          'Sequence Master',
+                          style: TextStyle(
+                            fontFamily:
+                                'Roboto', // Remplace par une autre police si souhaité
+                            fontSize: 24, // Taille de la police
+                            fontWeight: FontWeight.normal, // Epaisseur du texte
+                            color: AppColors.iconsPrimary, // Couleur du texte
+                            letterSpacing: 2, // Espacement des lettres
+                            wordSpacing: 3, // Espacement des mots
                           ),
+                          textAlign: TextAlign.center, // Alignement du texte
+                        ),
                       ),
                     ],
                   ),
@@ -343,6 +396,200 @@ class _HomeMemoryPageState extends State<HomeMemoryPage> {
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: Row(
+        //* Home
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: InkResponse(
+              onTap: () {
+                // Action à exécuter lors du tapotement
+                print("You are ready on Home");
+              },
+              containedInkWell:
+                  true, // Assure que l'ondulation reste contenue dans les limites du widget enfant
+              highlightShape: BoxShape
+                  .rectangle, // Forme de l'effet d'ondulation, ici un rectangle
+              highlightColor: Colors.black87.withOpacity(
+                  1), // Couleur de surbrillance lors de l'interaction
+              splashColor: Colors.pink
+                  .withOpacity(1), // Couleur de l'effet d'ondulation (splash)
+              radius:
+                  2.0, // Rayon de l'effet d'ondulation (utile si la forme est circulaire)
+              borderRadius:
+                  BorderRadius.circular(15), // Coin arrondis pour l'ondulation
+              enableFeedback:
+                  true, // Active le retour haptique ou sonore, selon la plateforme
+              onTapCancel: () {
+                // Action à exécuter si l'utilisateur annule le tapotement
+              },
+              onTapDown: (TapDownDetails details) {
+                // Action à exécuter lorsque l'utilisateur commence à tapoter
+              },
+              onTapUp: (TapUpDetails details) {
+                // Action à exécuter lorsque l'utilisateur relâche le tapotement
+              },
+              child: Container(
+                width: 68,
+                height: 68,
+                decoration: const BoxDecoration(
+                  color: AppColors.fondApp,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      blurStyle: BlurStyle.inner,
+                      color: Color.fromARGB(80, 255, 255, 255),
+                      offset: Offset(0, -3),
+                      blurRadius: 4,
+                      spreadRadius: -0.5,
+                    ),
+                    BoxShadow(
+                      blurStyle: BlurStyle.outer,
+                      color: Color.fromARGB(80, 0, 0, 0),
+                      offset: Offset(0, 3),
+                      blurRadius: 4,
+                      spreadRadius: 0,
+                    ),
+                  ],
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.home_filled,
+                    color: AppColors.iconsPrimary,
+                    size: 48,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: InkResponse(
+              onTap: () {
+                // Action à exécuter lors du tapotement
+                print("You are ready on Home");
+              },
+              containedInkWell:
+                  true, // Assure que l'ondulation reste contenue dans les limites du widget enfant
+              highlightShape: BoxShape
+                  .rectangle, // Forme de l'effet d'ondulation, ici un rectangle
+              highlightColor: Colors.black87.withOpacity(
+                  1), // Couleur de surbrillance lors de l'interaction
+              splashColor: Colors.pink
+                  .withOpacity(1), // Couleur de l'effet d'ondulation (splash)
+              radius:
+                  2.0, // Rayon de l'effet d'ondulation (utile si la forme est circulaire)
+              borderRadius:
+                  BorderRadius.circular(15), // Coin arrondis pour l'ondulation
+              enableFeedback:
+                  true, // Active le retour haptique ou sonore, selon la plateforme
+              onTapCancel: () {
+                // Action à exécuter si l'utilisateur annule le tapotement
+              },
+              onTapDown: (TapDownDetails details) {
+                // Action à exécuter lorsque l'utilisateur commence à tapoter
+              },
+              onTapUp: (TapUpDetails details) {
+                // Action à exécuter lorsque l'utilisateur relâche le tapotement
+              },
+              child: Container(
+                width: 68,
+                height: 68,
+                decoration: const BoxDecoration(
+                  color: AppColors.fondApp,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      blurStyle: BlurStyle.inner,
+                      color: Color.fromARGB(80, 255, 255, 255),
+                      offset: Offset(0, -3),
+                      blurRadius: 4,
+                      spreadRadius: -0.5,
+                    ),
+                    BoxShadow(
+                      blurStyle: BlurStyle.outer,
+                      color: Color.fromARGB(80, 0, 0, 0),
+                      offset: Offset(0, 3),
+                      blurRadius: 4,
+                      spreadRadius: 0,
+                    ),
+                  ],
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.add,
+                    color: AppColors.iconsPrimary,
+                    size: 65,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: InkResponse(
+              onTap: () {
+                // Action à exécuter lors du tapotement
+                print("You are ready on Home");
+              },
+              containedInkWell:
+                  true, // Assure que l'ondulation reste contenue dans les limites du widget enfant
+              highlightShape: BoxShape
+                  .rectangle, // Forme de l'effet d'ondulation, ici un rectangle
+              highlightColor: Colors.black87.withOpacity(
+                  1), // Couleur de surbrillance lors de l'interaction
+              splashColor: Colors.pink
+                  .withOpacity(1), // Couleur de l'effet d'ondulation (splash)
+              radius:
+                  2.0, // Rayon de l'effet d'ondulation (utile si la forme est circulaire)
+              borderRadius:
+                  BorderRadius.circular(15), // Coin arrondis pour l'ondulation
+              enableFeedback:
+                  true, // Active le retour haptique ou sonore, selon la plateforme
+              onTapCancel: () {
+                // Action à exécuter si l'utilisateur annule le tapotement
+              },
+              onTapDown: (TapDownDetails details) {
+                // Action à exécuter lorsque l'utilisateur commence à tapoter
+              },
+              onTapUp: (TapUpDetails details) {
+                // Action à exécuter lorsque l'utilisateur relâche le tapotement
+              },
+              child: Container(
+                width: 68,
+                height: 68,
+                decoration: const BoxDecoration(
+                  color: AppColors.fondApp,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      blurStyle: BlurStyle.inner,
+                      color: Color.fromARGB(80, 255, 255, 255),
+                      offset: Offset(0, -3),
+                      blurRadius: 4,
+                      spreadRadius: -0.5,
+                    ),
+                    BoxShadow(
+                      blurStyle: BlurStyle.outer,
+                      color: Color.fromARGB(80, 0, 0, 0),
+                      offset: Offset(0, 3),
+                      blurRadius: 4,
+                      spreadRadius: 0,
+                    ),
+                  ],
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.person_rounded,
+                    color: AppColors.iconsPrimary,
+                    size: 48,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
